915a7618f4d03c3c12779f2053caba1f3eb68aba
marijanciceran25 <115954072+marijanciceranUNIPU@users.noreply.github.com>
2022-12-30 12:34:05 +0100

    Provjera popunjenosti baze i upis podataka
    + pandas čitanje file-a

--- projekt.py
+++ projekt.py
@@ -1,4 +1,11 @@
 import sqlite3
+import json
+import aiosqlite
+import asyncio
+import aiohttp
+import aiofiles
+import pandas as pd
+from aiohttp import web
 
 conn = sqlite3.connect('dbase.db')
 cursor = conn.cursor()
@@ -11,11 +18,35 @@ if counter_check == 0:
 else:
     print("Database is NOT empty.")
 
-query = 'SELECT id, username, ghublink, filename FROM mini_projekt_db'
+routes = web.RouteTableDef()
 
-results = cursor.execute(query)
+@routes.get("/JSON_Data")
+async def json_data(request):
+    try:
+        async with open('file-000000000040.json', 'r') as file_data:
+            read_data = { await file_data.readline() for _ in range(10)}
+            whole_data = [json.loads(line) for line in read_data]
+            database = []
+            async with aiosqlite.connect("dbase.db") as db:
+                for item in whole_data:
+                    db_item = {}
+                    db_item["username"] = item["repo_name"].rsplit("/", 1)[0]
+                    db_item["ghublink"] = "https://github.com/" + item["repo_name"] + ".com"
+                    db_item["filename"] = item["path"].rsplit("/", 1)[1]
+                    database.append(db_item)
+                    await db.execute(
+                        "INSERT INTO mini_projekt_db (username, ghublink, filename) VALUES (?,?,?)",
+                        (
+                            db_item["username"], db_item["ghlink"], db_item["filename"]))
+                    await db.commit()
+                async with db.execute("SELECT * FROM mini_projekt_db") as cur:
+                    result = len(await cur.fetchall())
+            message = {"status": "ok", "data": {"numberOfRowsInTable": result}}
+            return web.json_response(message, status=200)
 
-for row in results:
-    print(row)
+    except Exception as e:
+        return web.json_response({"Error": str(e)}, status=500)
 
-conn.close()
+app = web.Application()
+app.router.add_routes(routes)
+web.run_app(app, port=8081)
